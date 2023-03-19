37b930bff7477cfbdc4675aa9c30219a6d27bbd4
nilesh-swivel <nilesh@swivelgroup.com.au>
2023-03-19 14:09:15 +0530

    Added title

--- packages/client/src/components/Home/ElementalPicker.tsx
+++ packages/client/src/components/Home/ElementalPicker.tsx
@@ -18,31 +18,36 @@ export default function ElementalPicker() {
     useRecoilState(selectedElementAtom);
 
   return (
-    <div className="w-full max-w-[400px] flex items-center justify-between py-3 px-4 my-3">
-      {elements.map((element, index) => (
-        <div
-          key={index}
-          onClick={() => {
-            if (selectedElement !== element) {
-              setSelectedElement(element);
-            } else {
-              setSelectedElement("all");
-            }
-          }}
-        >
-          <img
-            src={elementalImageFilter[element]}
-            alt={element}
-            className="w-[28px]"
-            style={{
-              filter:
-                selectedElement === element || selectedElement === "all"
-                  ? "none"
-                  : "grayscale(100%)",
+    <div className="w-full pt-3 mt-3 mx-2 px-2 flex flex-col items-center">
+      <div className="w-full flex items-center justify-center xl:mb-4">
+        <h1 className="font-bold my-2 text-3xl xl:text-5xl px-4">Characters</h1>
+      </div>
+      <div className="w-full flex items-center justify-between pb-3 px-4 mb-3 max-w-[400px]">
+        {elements.map((element, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              if (selectedElement !== element) {
+                setSelectedElement(element);
+              } else {
+                setSelectedElement("all");
+              }
             }}
-          />
-        </div>
-      ))}
+          >
+            <img
+              src={elementalImageFilter[element]}
+              alt={element}
+              className="w-[28px]"
+              style={{
+                filter:
+                  selectedElement === element || selectedElement === "all"
+                    ? "none"
+                    : "grayscale(100%)",
+              }}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
