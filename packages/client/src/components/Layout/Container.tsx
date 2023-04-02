414f116c092e60be1ba1cf830b05c2e3164dc7b3
nilesh-swivel <nilesh@swivelgroup.com.au>
2023-04-02 15:04:13 +0530

    UI code cleanup

--- packages/client/src/components/Layout/Container.tsx
+++ packages/client/src/components/Layout/Container.tsx
@@ -2,15 +2,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 type Props = {
-  children: JSX.Element[] | JSX.Element;
+  children: React.ReactNode;
 };
 
-const Container: React.FC<Props> = ({ children }) => (
-  <div className="bg-slate-100 dark:bg-gray-900 dark:text-white overflow-x-hidden min-h-screen w-full flex flex-col items-center justify-start">
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const Container: React.FC<Props> = ({ children }) => {
+  if (typeof children !== "object") {
+    throw new Error("Container component only accepts React children.");
+  }
+
+  return (
+    <div className="bg-slate-100 dark:bg-gray-900 dark:text-white overflow-x-hidden min-h-screen w-full flex flex-col items-center justify-start">
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 export default Container;
--- packages/client/src/functions/ParseEnkaText.ts
+++ packages/client/src/functions/ParseEnkaText.ts
@@ -1,22 +1,30 @@
 function parseText(inputText: string) {
+  let modifiedText = inputText;
+
   // Replace color codes with HTML style tags
-  let modifiedText = inputText.replace(
+  modifiedText = modifiedText.replace(
     /<color=([^>]+)>/g,
     '<b style="color:$1">'
   );
   modifiedText = modifiedText.replace(/<\/color>/g, "</b>");
 
+  // Replace {LAYOUT_MOBILE#Tap}{LAYOUT_PC#Press}{LAYOUT_PS#Press} with "Mobile:Tap, PS/PC: Press"
   modifiedText = modifiedText.replace(
     /\{LAYOUT_MOBILE#Tap\}\{LAYOUT_PC#Press\}\{LAYOUT_PS#Press\}/g,
     "Mobile:Tap, PS/PC: Press"
   );
 
+  // Replace newlines with <br/>
   modifiedText = modifiedText.replace(/(\r\n|\n|\\n|\r)/gm, "<br/>");
 
-  //Removes unnecessary # symbols from the modified text
-  const regex = /#(?!([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))/g;
+  // Remove unnecessary # symbols
+  modifiedText = modifiedText.replace(
+    /#(?!([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))/g,
+    ""
+  );
 
-  modifiedText = modifiedText.replace(regex, "");
+  // Remove <br/> tags from the beginning of the text
+  modifiedText = modifiedText.replace(/^<br\/>/g, "");
 
   return modifiedText;
 }
