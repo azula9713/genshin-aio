cf9d2a6c12cae350f84b46a0d9fd5c7b4e7a6e50
nilesh-swivel <nilesh@swivelgroup.com.au>
2023-04-08 04:17:48 +0530

    Error handling

--- packages/client/src/components/Character/PC/CharacterTalentDetails.tsx
+++ packages/client/src/components/Character/PC/CharacterTalentDetails.tsx
@@ -6,19 +6,27 @@ type Props = {
 };
 
 export default function CharacterTalentDetails({ selectedTalent }: Props) {
+  if (selectedTalent) {
+    return (
+      <div className="w-full flex flex-col items-start justify-start text-white text-xl bg-slate-700 px-8 py-4 rounded-lg">
+        <h2 className="font-algoindeEnka text-3xl mb-4">
+          {selectedTalent.name} {selectedTalent.isPassive ? "(Passive)" : ""}
+        </h2>
+        <div>
+          <div
+            className="w-ful"
+            dangerouslySetInnerHTML={{
+              __html: parseText(selectedTalent.description || ""),
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="w-full flex flex-col items-start justify-start text-white text-xl bg-slate-700 px-8 py-4 rounded-lg">
-      <h2 className="font-algoindeEnka text-3xl mb-4">
-        {selectedTalent?.name} {selectedTalent?.isPassive ? "(Passive)" : ""}
-      </h2>
-      <div>
-        <div
-          className="w-ful"
-          dangerouslySetInnerHTML={{
-            __html: parseText(selectedTalent?.description || ""),
-          }}
-        />
-      </div>
+      <h2 className="font-algoindeEnka text-3xl mb-4">Select a talent</h2>
     </div>
   );
 }
